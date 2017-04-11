f962d6c5d25317eedf9fbfb4d890f8b31bef5241
vinicius0809 <vinicius0809@gmail.com>
2017-04-11 13:33:26 -0300

    Update IA.py

--- IA.py
+++ IA.py
@@ -1,7 +1,7 @@
 #variaveis globais
-NUMBER_MOVES = 2
+NUMBER_MOVES = 15
 NUMBER_GENES = 9
-NUMBER_ORGANISMS = 5
+NUMBER_ORGANISMS = 15
 MUTATION_RATE = 0.001
 MAXIMUM_FITNESS = NUMBER_GENES
 modelOrganism = [0,1,2,3,4,5,6,7,8]
@@ -12,10 +12,12 @@ from random import *
 
 class Individuo:
 
-    def __init__(self,numero,gene):
+    def __init__(self,numero,gene,fitness):
         self.id = numero
         self.gene = gene
         self.historico = []
+        self.fitness = fitness
+
 
 def movimento(individuo):
     branco = 0
@@ -37,6 +39,11 @@ def movimento(individuo):
                 branco += 3
             else:
                 sair = False
+    #NUMBER_MOVES += 1
+
+def ordena_fitness(geracao):
+    from operator import attrgetter
+    geracao.sort(key=lambda a: a.fitness)
 
 
 def move_direita(organism,i):
@@ -85,8 +92,9 @@ def DoOneRun():
 def Fitness(individuo):
     fitness = 0
     for gene in range (0,NUMBER_GENES):
-        if(individuo[gene] == modelOrganism[gene]):
-             fitness += 1
+        fitness += abs(individuo[gene]-gene)
+    if (fitness == 0):
+        print("Sequencia para resolver: ",individuo.historico)
     return fitness
 
 def InitializeOrganisms():
@@ -105,26 +113,26 @@ def InitializeOrganisms():
                 inicialModel.append(aux)
                 i=False
     for x in range (0,NUMBER_ORGANISMS):
-        geracao.append(Individuo(x,inicialModel))
+        copiaInicialModel = inicialModel[:]
+        geracao.append(Individuo(x,copiaInicialModel,0))
 
     print(inicialModel[0:NUMBER_GENES])
     print(modelOrganism[0:NUMBER_GENES])
-    print("Valor do fitness: ",Fitness(inicialModel))
-    print("Individuos")
+    print("Valor do fitness inicial: ",Fitness(inicialModel))
+
+    print("\nIndividuos após movimentos\n")
+    for organism in range(0,NUMBER_ORGANISMS):
+        movimento(geracao[organism])
+        print(geracao[organism].gene)
+        geracao[organism].fitness = Fitness(geracao[organism].gene)
+        print("Valor fitness: ", geracao[organism].fitness,"\nID: ",geracao[organism].id)
+
+
+    print("\nIndividuos com melhor fitness após movimentos\n")
+    ordena_fitness(geracao)
     for organism in range(0,NUMBER_ORGANISMS):
         print(geracao[organism].gene)
-
-
-    movimento(geracao)
-    print(geracao[0].historico)
-    print(geracao[0].gene)
-    print(geracao[4].gene)
-    print("_____________________")
-    geracao[4].gene[i],geracao[4].gene[i+3] = geracao[4].gene[i+3], geracao[4].gene[i]
-    print(geracao[0].gene)
-    print(geracao[4].gene)
-    #for organism in range(0,NUMBER_ORGANISMS):
-    #    print(geracao[organism].gene)
+        print("Valor fitness: ", geracao[organism].fitness,"\nID: ",geracao[organism].id,"\n")
 
 
 DoOneRun()
